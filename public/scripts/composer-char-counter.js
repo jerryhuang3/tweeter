b43026e3fda4731a877305cc8e1acf4d76e90704
jerryhuang3 <jerryhuang@utexas.edu>
2019-03-18 21:27:09 +0000

    added CSS class to handle too many characters inputted

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,15 @@
+$(document).ready(function() {
+    let maxInput = parseInt($("span.counter")[0].textContent);
+        $("textarea").keyup(function() {
+            let tweetChar = $(this).val().length;
+            const form = this.parentNode; // Traversing up the DOM
+            const count = form.children[2]; 
+            let remaining = maxInput - tweetChar;
+            $(".counter")[0].textContent = remaining;
+            if ($(".counter")[0].textContent < 0) {
+                $(".counter").addClass("error");
+            } else if ( $(".counter")[0].textContent >= 0) {
+                $(".counter").removeClass("error");
+            }
+        });
+});
