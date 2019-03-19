14e85e5e70c1d19f2af4edab802d798925bbccd1
jerryhuang3 <jerryhuang@utexas.edu>
2019-03-19 14:14:40 +0000

    Added renderTweets function that displays all previous tweets

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -6,39 +6,75 @@
 
 // Test / driver code (temporary). Eventually will get this from the server.
 $(document).ready(function() {
-const tweetData = {
-    "user": {
-        "name": "Newton",
-        "avatars": {
-            "small":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_50.png",
-            "regular": "https://vanillicon.com/788e533873e80d2002fa14e1412b4188.png",
-            "large":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_200.png"
+// Fake data taken from tweets.json
+    const data = [
+        {
+            "user": {
+                "name": "Newton",
+                "avatars": {
+                    "small": "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_50.png",
+                    "regular": "https://vanillicon.com/788e533873e80d2002fa14e1412b4188.png",
+                    "large": "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_200.png"
+                },
+                "handle": "@SirIsaac"
+            },
+            "content": {
+                "text": "If I have seen further it is by standing on the shoulders of giants"
+            },
+            "created_at": 1461116232227
         },
-        "handle": "@SirIsaac"
-    },
-    "content": {
-        "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-    "created_at": 1461116232227
-}
+        {
+            "user": {
+                "name": "Descartes",
+                "avatars": {
+                    "small": "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_50.png",
+                    "regular": "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc.png",
+                    "large": "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_200.png"
+                },
+                "handle": "@rd"
+            },
+            "content": {
+                "text": "Je pense , donc je suis"
+            },
+            "created_at": 1461113959088
+        },
+        {
+            "user": {
+                "name": "Johann von Goethe",
+                "avatars": {
+                    "small": "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_50.png",
+                    "regular": "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1.png",
+                    "large": "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_200.png"
+                },
+                "handle": "@johann49"
+            },
+            "content": {
+                "text": "Es ist nichts schrecklicher als eine tätige Unwissenheit."
+            },
+            "created_at": 1461113796368
+        }
+    ];
 
+    function renderTweets(tweets) {
+        for (let i = 0; i < tweets.length; i++) {
+            let $tweet = createTweets(tweets[i]);
+            $('#tweets-container').append($tweet);
+        }
+    }
 
-function createTweets(tweetData) {
-    let date = tweetData.created_at;
-    let $tweet = $("<article>").addClass("tweets");
-    $tweet.append( 
-        `<header>
+    function createTweets(tweetData) {
+        let date = tweetData.created_at;
+        let $tweet = $("<article>").addClass("tweets");
+        $tweet.append(
+            `<header>
             <h2 class="tweets">${tweetData.user.name}</h2>
             <span class="tweets">${tweetData.user.handle}</span>
         </header>
         <body>
             <p class="tweets">${tweetData.content.text}</body>
         <footer>${date}</footer>`);
-    $('#tweets-container').append($tweet);
-}
+        return $tweet;
+    }
 
-createTweets(tweetData);
-// Test / driver code (temporary)
- // to see what it looks like
- // to add it to the page so we can make sure it's got all the right elements, classes, etc.
+    renderTweets(data);
 });
