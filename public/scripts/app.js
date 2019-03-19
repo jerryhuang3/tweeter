91c1976d6f017b938be03f2d19f644990d6c2f1f
jerryhuang3 <jerryhuang@utexas.edu>
2019-03-19 22:24:23 +0000

    added function to fetch new tweets

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,4 +1,21 @@
 $(document).ready(function() {
+    function fetchTweets() {
+        $("form").submit(function(event) {
+            event.preventDefault();
+            let string = this.children[0].value.replace(/ /g, "");
+            if (this.children[0].value.length <= 140 && this.children[0].value.length > 0 && string !== "") {
+                $.post("/tweets", $(this).serialize(), function() {
+                    loadTweets();
+                });
+                $("#tweets-container").load("index.html .tweets");
+                
+            } else {
+                return alert("Error! Your tweet is either too long or empty!");
+            }
+        });
+    }
+    
+    
     function renderTweets(tweets) {
         for (let i = tweets.length - 1; i >= 0; i--) {
             let $tweet = createTweets(tweets[i]);
