d96dba956936399e3120f3a13520c1318c680019
jerryhuang3 <jerryhuang@utexas.edu>
2019-03-20 17:40:53 +0000

    added font awesome icons in tweet box

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -2,7 +2,6 @@ $(document).ready(function() {
     let maxInput = parseInt($("span.counter")[0].textContent);
         $("textarea").keyup(function() {
             let tweetChar = $(this).val().length;
-            console.log($(this).val());
             const form = this.parentNode; // Traversing up the DOM
             const count = form.children[2]; 
             let remaining = maxInput - tweetChar;
