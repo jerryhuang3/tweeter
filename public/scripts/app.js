a0334255e2f2a690242deb30ebc6006e99512ae8
jerryhuang3 <jerryhuang@utexas.edu>
2019-03-20 16:38:13 +0000

    add mongoDatabase into serve; refactored app functions

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -3,6 +3,26 @@ $(document).ready(function() {
         $(".new-tweet").slideToggle("fast");
         $("textarea").focus();
     });
+
+    $("form").submit(function(event) {
+        event.preventDefault();
+        let string = this.children[1].value.replace(/ /g, "");
+        if (this.children[1].value.length <= 140 && this.children[1].value.length > 0 && string !== "") {
+            $.post("/tweets", $(this).serialize(), function() {
+                $.getScript("/scripts/app.js");
+            });
+            
+            
+            $("#tweets-container").load("index.html .tweets");
+            $(this[0]).val("");
+            $(".error").addClass("hidden");
+            $("textarea").removeClass("textarea-error");
+        } else {
+            $(".error").removeClass("hidden");
+            $("textarea").addClass("textarea-error");
+            return;
+        }
+    });
     
     function escape(str) {
         var div = document.createElement('div');
@@ -11,22 +31,22 @@ $(document).ready(function() {
     }
     
     function renderTweets(tweets) {
-        for (let i = tweets.length - 1; i >= 0; i--) {
-            let $tweet = createTweets(tweets[i]);
+        for (let i = tweets.length -1; i >= 0; i--) {
+            $tweet = createTweets(tweets[i]);
             $('#tweets-container').append($tweet);
         }
     }
 
     function createTweets(tweetData) {
-        let date = tweetData.created_at;
         let $tweet = $("<article>").addClass("tweets");
         $tweet.append(
             `<header>
+                <img src="${escape(tweetData.user.avatars.large)}">
                 <h2>${escape(tweetData.user.name)}</h2>
                 <span>${escape(tweetData.user.handle)}</span>
             </header>
                 <p>${escape(tweetData.content.text)}</p>
-            <footer>${date}</footer>`);
+            <footer>${moment(tweetData.created_at).fromNow()}</footer>`);
         return $tweet;
     }
     
--- server/index.js
+++ server/index.js
@@ -7,11 +7,25 @@ const express       = require("express");
 const bodyParser    = require("body-parser");
 const app           = express();
 
+
+
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
+const {MongoClient} = require("mongodb");
+const MONGODB_URI = "mongodb://localhost:27017/tweeter";
+
+MongoClient.connect(MONGODB_URI, (err, db) => {
+  if (err) {
+    console.error(`Failed to connect: ${MONGODB_URI}`);
+    throw err;
+  }
+
+
+  // We have a connection to the "tweeter" db, starting here.
+  console.log(`Connected to mongodb: ${MONGODB_URI}`);
 
 // The in-memory database of tweets. It's a basic object with an array in it.
-const db = require("./lib/in-memory-db");
+// const MONGODB_URI = require("./lib/in-memory-db");
 
 // The `data-helpers` module provides an interface to the database of tweets.
 // This simple interface layer has a big benefit: we could switch out the
@@ -31,4 +45,6 @@ app.use("/tweets", tweetsRoutes);
 
 app.listen(PORT, () => {
   console.log("Example app listening on port " + PORT);
+  
+});
 });
